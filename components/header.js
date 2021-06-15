0acd8f69edc513d795395e295f33ba4bb0eb9d3b
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-06-15 12:08:31 +0800

    add items

--- components/header.js
+++ components/header.js
@@ -2,28 +2,39 @@
 import { Flex, NavLink, Text } from "@theme-ui/components";
 import Link from "next/link";
 
+const navItems = [
+  { label: "Projects", link: "#project" },
+  { label: "About", link: "/about" },
+  { label: "Send mail", link: "mailto:hey@yuchengkuo.com?subject=Hi!" },
+];
+
 const Header = () => {
   return (
     <header>
-      <Flex as="nav" variant="layout.nav">
-        <Flex sx={{ flex: `1 1 auto` }}>
+      <Flex
+        as="nav"
+        variant="layout.nav"
+        sx={{
+          flexDirection: ["column", "row"],
+          alignItems: ["center", "unset"],
+        }}
+      >
+        <Flex sx={{ flex: `1 1 auto`, mb: [5, 0] }}>
           <Link href="/">
-            <NavLink sx={{ "> *": { mr: 3 } }}>
+            <NavLink
+              sx={{ "> *": { mr: 2 }, display: "flex", alignItems: "center" }}
+            >
               <LogoSvg />
               <Text>yucheng kuo.</Text>
             </NavLink>
           </Link>
         </Flex>
         <Flex sx={{ justifyContent: "space-between", gap: 4 }}>
-          <Link href="">
-            <NavLink>Projects</NavLink>
-          </Link>
-          <Link href="/about">
-            <NavLink>About</NavLink>
-          </Link>
-          <Link href="mailto:hey@yuchengkuo.com?subject=Hi">
-            <NavLink>Send mail</NavLink>
-          </Link>
+          {navItems.map((item, i) => (
+            <Link href={item.link} key={i}>
+              <NavLink>{item.label}</NavLink>
+            </Link>
+          ))}
         </Flex>
       </Flex>
     </header>
@@ -42,7 +53,7 @@ const LogoSvg = () => {
       <path
         d="M13 1V25M13 1H1L7 13L1 25H13M13 1H25M13 25H25M13 25L25 1M31 13.5L37 1H25M31 13.5L37 25H25M31 13.5L25 25"
         stroke="#BDBEC1"
-        strokeWidth="3"
+        strokeWidth="2"
         strokeLinejoin="round"
       />
     </svg>
