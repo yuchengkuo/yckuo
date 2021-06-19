0216a97d2b57631df9208ac3085267feaa090ba0
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-06-19 09:31:50 +0800

    add playing

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,33 @@
+/** @jsxImportSource theme-ui */
+import { LogoSvg } from "./Header";
+import { Flex, Text } from "theme-ui";
+import NowPlaying from "./NowPlaying";
+
 export default function Footer() {
   return (
     <footer>
-      <h3>This is footer</h3>
+      <Flex
+        as="nav"
+        variant="layout.nav"
+        mb={8}
+        sx={{
+          justifyContent: "space-between",
+          flexDirection: ["column", "row"],
+          alignItems: ["center", "unset"],
+        }}
+      >
+        <Flex sx={{ alignItems: "center", mb: [5, 0] }}>
+          <LogoSvg />
+          <Text
+            as="p"
+            ml={2}
+            sx={{ fontSize: 1, fontWeight: 600, color: "secondaryText" }}
+          >
+            © yucheng kuo. 2021-
+          </Text>
+        </Flex>
+        <NowPlaying />
+      </Flex>
     </footer>
   );
 }
