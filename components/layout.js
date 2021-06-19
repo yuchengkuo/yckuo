566061ecddb460d0e86ee673367e8a6249ba9f8c
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-06-19 10:34:16 +0800

    add footer

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
     <main>
       <Header />
       {children}
+      <Footer />
     </main>
   );
 };
