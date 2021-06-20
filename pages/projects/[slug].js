1b9b0116348b7a788e7e59b261d213d0bf18394b
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-06-21 00:21:06 +0800

    add hero

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -8,7 +8,7 @@ import fs from "fs";
 import path from "path";
 
 import Layout from "components/Layout";
-import { Grid, Container, Themed } from "theme-ui";
+import { Grid, Container, Themed, Flex } from "theme-ui";
 
 import GoTop from "components/GoTop";
 import Carousel from "components/Carousel";
@@ -22,13 +22,71 @@ const components = {
 const ProjectTemplate = ({ mdxSource, frontMatter }) => {
   return (
     <Layout>
-      <Container as="section" variant="hero">
+      <Container as="section" variant="hero" sx={{ position: "relative" }}>
         <Themed.h1 sx={{ maxWidth: 640, mt: 12 }}>
           {frontMatter.title}
         </Themed.h1>
+        <p sx={{ variant: "text.caps", color: "secondaryText" }}>
+          {frontMatter.subtitle}
+        </p>
+        <svg
+          width="431"
+          height="590"
+          viewBox="0 0 431 590"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          sx={{ position: "absolute", top: -106, left: `calc(50% + 390px)` }}
+        >
+          <path d="M430 0L215.5 429H1" stroke="#FCB33D" />
+          <path
+            d="M80.2381 269L1.43957 428.112C1.16258 428.672 1.16258 429.328 1.43957 429.888L80.2381 589"
+            stroke="#FCB33D"
+          />
+        </svg>
+      </Container>
+      <Container
+        variant="section"
+        sx={{ display: "flex", justifyContent: "space-between", gap: 7 }}
+      >
+        {frontMatter.info?.map((item, i) => (
+          <Flex
+            key={i}
+            sx={{
+              flexDirection: "column",
+              border: "1px solid",
+              borderRadius: 15,
+              p: 4,
+              width: `100%`,
+            }}
+          >
+            <h4
+              sx={{
+                fontSize: 3,
+                fontWeight: "heading",
+                mt: 0,
+                mb: 4,
+                color: "secondaryText",
+              }}
+            >
+              {item.title}
+            </h4>
+            {item.content.map((text, i) => (
+              <p
+                key={i}
+                sx={{ fontSize: 2, m: 0, mb: 3, color: "secondaryText" }}
+              >
+                {text}
+              </p>
+            ))}
+          </Flex>
+        ))}
       </Container>
       <Grid as="section" variant="article">
-        <Container as="article" variant="section" sx={{ gridColumn: 2 }}>
+        <Container
+          as="article"
+          variant="section"
+          sx={{ gridColumn: 2, section: { my: 10 } }}
+        >
           <MDXRemote {...mdxSource} components={components} />
         </Container>
         <GoTop />
