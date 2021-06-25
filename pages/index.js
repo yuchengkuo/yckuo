bdcd40134593f176bba0660b5fbd7a09ebd2b955
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-06-25 19:45:35 +0800

    update dir

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Flex, Container, Button, Themed, Text } from "theme-ui";
 import Link from "next/link";
 import Hero from "components/Hero";
 import ProjectCard from "components/ProjectCard";
-import { getAllProjectsData } from "lib/projects";
+import { getAllProjectsData } from "utlis/projects";
 
 const Home = ({ allProjectsData }) => {
   return (
