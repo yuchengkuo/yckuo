09d238ef83af19d1d6214d4a2d29502f5afb4d97
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-06-25 23:51:15 +0800

    add hr

--- components/Footer.js
+++ components/Footer.js
@@ -12,18 +12,35 @@ export default function Footer() {
         mb={8}
         sx={{
           justifyContent: "space-between",
-          flexDirection: ["column-reverse", "row"],
+          flexDirection: ["column-reverse", null],
           alignItems: ["center", "unset"],
+          position: "relative",
+          ":before": {
+            position: "absolute",
+            content: "''",
+            height: 1,
+            width: `calc(100% - 80px)`,
+            top: 0,
+            left: 6,
+            backgroundColor: "altText",
+          },
         }}
       >
-        <Flex sx={{ alignItems: "center", mt: [5, 0] }}>
+        <Flex sx={{ alignItems: "center", mt: 6 }}>
           <LogoSvg />
           <Text
             as="p"
             ml={2}
             sx={{ fontSize: 1, fontWeight: 600, color: "secondaryText" }}
           >
-            © yucheng kuo. 2021-
+            made by yucheng kuo. &#169; 2021
+          </Text>
+          <Text
+            as="p"
+            ml="auto"
+            sx={{ fontSize: 1, fontWeight: 600, color: "secondaryText" }}
+          >
+            TW, KHH
           </Text>
         </Flex>
         <NowPlaying />
