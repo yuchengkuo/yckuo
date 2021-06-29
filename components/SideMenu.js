eaabe7be0be69a6cf9721d5b12a6191b723484ad
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-06-29 15:50:22 +0800

    update style

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,36 +1,81 @@
 /** @jsxImportSource theme-ui */
-import { Flex, Button } from "theme-ui";
+import { transparentize } from "@theme-ui/color";
+import { Button } from "theme-ui";
+import { motion, useViewportScroll, AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+
 export default function SideMenu({ ids }) {
+  const { scrollYProgress } = useViewportScroll();
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    scrollYProgress.onChange((current) => {
+      current > 0.05
+        ? current < 0.98
+          ? setIsShow(true)
+          : setIsShow(false)
+        : setIsShow(false);
+    });
+  });
+
   return (
-    <Flex
-      sx={{
-        flexDirection: "column",
-        position: "fixed",
-        left: 60,
-        top: 5,
-        width: 200,
-      }}
-    >
-      <h4 sx={{ variant: "text.caps" }}>content</h4>
-      <ul sx={{ listStyle: "none", p: 0 }}>
-        {ids.map((item, i) => (
-          <li key={i}>
-            <Button
-              variant="secondary"
-              onClick={() => {
-                const article = document.getElementById("article").offsetTop;
-                window.scrollTo({
-                  top:
-                    document.getElementById(item.id).offsetTop + article - 50,
-                  behavior: "smooth",
-                });
-              }}
-            >
-              {item.title}
-            </Button>
-          </li>
-        ))}
-      </ul>
-    </Flex>
+    <AnimatePresence>
+      {isShow && (
+        <motion.div
+          sx={{
+            display: ["none", null, "flex"],
+            flexDirection: "column",
+            position: "fixed",
+            left: `calc(50% + 640px)`,
+            top: 12,
+            maxWidth: 160,
+            zIndex: 9,
+            p: 2,
+            backgroundColor: transparentize("background", 0.6),
+            borderRadius: 15,
+          }}
+          initial={{ x: 20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <h4
+            sx={{ variant: "text.caps", p: 2, my: 2, color: "secondaryText" }}
+          >
+            content
+          </h4>
+          <ul sx={{ listStyle: "none", p: 0, m: 0 }}>
+            {ids.map((item, i) => (
+              <li key={i}>
+                <Button
+                  onClick={() => {
+                    const article =
+                      document.getElementById("article").offsetTop;
+                    window.scrollTo({
+                      top:
+                        document.getElementById(item.id).offsetTop +
+                        article -
+                        50,
+                      behavior: "smooth",
+                    });
+                  }}
+                  sx={{
+                    fontSize: 0,
+                    fontWeight: 400,
+                    color: "altText",
+                    backgroundColor: "transparent",
+                    lineHeight: "heading5",
+                    textAlign: "start",
+                    p: 2,
+                    mb: 1,
+                  }}
+                >
+                  {item.title}
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
