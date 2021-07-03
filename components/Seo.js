11bbd3920e8bcc3629d653b15e1de2fcbe4b494a
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-07-03 12:46:05 +0800

    add seo

---
+++ components/Seo.js
@@ -0,0 +1,44 @@
+import { NextSeo, DefaultSeo as NextDefaultSeo, ArticleJsonLd } from "next-seo";
+import config from "utlis/config";
+
+export default function Seo({ path, title, description, image }) {
+  return (
+    <>
+      <NextSeo
+        title="title"
+        titleTemplate="titleTemplate"
+        description="description about the website."
+      />
+      <ArticleJsonLd
+        url={path}
+        title={title}
+        authorName={config.author.name}
+        datePublished={new Date().toISOString()}
+        description={description}
+        images={image}
+        publisherName={config.author.name}
+        publisherLogo={config.image}
+      />
+    </>
+  );
+}
+
+export function DefaultSeo() {
+  return (
+    <NextDefaultSeo
+      title={config.title}
+      defaultTitle={config.title}
+      description={config.description}
+      canonical={config.url}
+      openGraph={{
+        type: "website",
+        title: config.title,
+        url: config.url,
+        description: config.description,
+        images: [
+          { url: config.image, alt: config.title, width: 1920, height: 960 },
+        ],
+      }}
+    />
+  );
+}
---
+++ utlis/config.js
@@ -0,0 +1,21 @@
+export default {
+  title: "Yu-Cheng Kuo",
+  keywords: [
+    "design",
+    "engineering",
+    "UI",
+    "UX",
+    "React",
+    "portfolio",
+    "nextjs",
+  ],
+  url: "yuchengkuo.com",
+  siteUrl: "yuchengkuo.com/",
+  sitLanguage: "en",
+  author: {
+    name: "YuCheng Kuo",
+  },
+  description:
+    "Hi, I'm YuCheng Kuo, a self-taught designer and engineer from Taiwan, and I likes interface, design, UX, react, web and many other!",
+  image: "/static/og-image.png",
+};
