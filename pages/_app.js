ad5d3be82a6594fd2be3e2f735511ce6a04f6bb0
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-07-04 16:29:56 +0800

    add seo

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,16 @@ import theme from "../utlis/theme";
 import components from "../utlis/components";
 import "../public/font/font.css";
 import "normalize.css";
+import { DefaultSeo } from "components/Seo";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme} components={components}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <DefaultSeo />
+      <ThemeProvider theme={theme} components={components}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
+          {/* <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
@@ -22,7 +22,32 @@ export default class MyDocument extends Document {
                   });
                 `,
             }}
+          /> */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/favicons/apple-touch-icon.png"
           />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/favicons/safari-pinned-tab.svg"
+            color="#fcb33d"
+          />
+          <meta name="msapplication-TileColor" content="#fcb33d" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
 
         <body>
