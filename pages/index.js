9f3a25a591ad7ce52d1b9b6b2690119851f83bb5
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-07-25 13:07:49 +0800

    update dir

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 /** @jsxImportSource theme-ui */
-import Layout from "components/Layout";
 import { Flex, Container, Button, Themed, Text } from "theme-ui";
 import Link from "next/link";
 import Hero from "components/Hero";
+import Layout from "components/Layout";
 import ProjectCard from "components/ProjectCard";
 import { getAllProjectsData } from "utlis/projects";
 
