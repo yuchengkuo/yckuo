3398531b63d232c2a193e4aac1bb7f990302a643
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-11-20 12:34:50 +0800

    add preact

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ module.exports = {
   images: {
     domains: ["i.scdn.co", "lastfm.freetls.fastly.net"],
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
   async rewrites() {
     return [
       {
