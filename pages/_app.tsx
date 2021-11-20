646b54af2c0e70d1d31cd63f2fa7e61def6c90d5
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-11-20 23:21:30 +0800

    update layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,21 @@
-//@ts-nocheck
-import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
-import "normalize.css";
-import { DefaultSeo } from "@components/Seo";
-import { globalStyles } from "@styles/global";
-import { lightTheme } from "stitches.config";
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import 'normalize.css';
+import { DefaultSeo } from '@components/Seo';
+import { globalStyles } from '@styles/global';
+import { lightTheme } from 'stitches.config';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
   return (
     <>
       <DefaultSeo />
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        value={{ light: lightTheme.className, dark: "dark" }}
-      >
+      <ThemeProvider attribute="class" value={{ light: lightTheme.className, dark: 'dark' }}>
+        <Header />
         <Component {...pageProps} />
+        <Footer />
       </ThemeProvider>
     </>
   );
