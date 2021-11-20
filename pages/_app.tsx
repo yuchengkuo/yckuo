56f0df17f5a2207fe685f3b4b48f80b326870230
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-11-20 23:47:15 +0800

    remove files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,15 @@ import { globalStyles } from '@styles/global';
 import { lightTheme } from 'stitches.config';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import Cursor from '@components/Cursor';
+import { Menu } from '@components/Menu';
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
   return (
     <>
+      <Cursor />
+      {/* <Menu /> */}
       <DefaultSeo />
       <ThemeProvider attribute="class" value={{ light: lightTheme.className, dark: 'dark' }}>
         <Header />
