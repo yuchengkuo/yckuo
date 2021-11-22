e7817108803a8e3961ef8fd1d00c2733e80aba6c
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-11-22 22:55:22 +0800

    modify projects dir

--- utils/mdx.ts
+++ utils/mdx.ts
@@ -1,29 +1,26 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import readingTime from "reading-time";
-import { Frontmatter } from "types/types";
-import { bundleMDX } from "mdx-bundler";
-import { remarkSectionize } from "./remark-sectionize";
-import getHeadings from "./getHeadings";
-import { getImageBlurData } from "./getImageBlurData";
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import readingTime from 'reading-time';
+import { Frontmatter } from 'types/types';
+import { bundleMDX } from 'mdx-bundler';
+import { remarkSectionize } from './remark-sectionize';
+import getHeadings from './getHeadings';
+import { getImageBlurData } from './getImageBlurData';
 
 const ROOT = process.cwd();
 
 export const getAllFrontmatter = (sourcePath: string) => {
-  const PATH = path.join(ROOT, sourcePath);
+  const PATH = path.join(ROOT, 'data', sourcePath);
   const paths = fs.readdirSync(PATH);
   return paths
     .map((fileName) => {
-      const source = fs.readFileSync(
-        path.join(ROOT, sourcePath, fileName),
-        "utf8"
-      );
+      const source = fs.readFileSync(path.join(ROOT, sourcePath, fileName), 'utf8');
       const { data, content } = matter(source);
 
       return {
         ...(data as Frontmatter),
-        slug: fileName.replace(".mdx", ""),
+        slug: fileName.replace('.mdx', ''),
         readingTime: readingTime(content),
       } as Frontmatter;
     })
@@ -31,32 +28,29 @@ export const getAllFrontmatter = (sourcePath: string) => {
 };
 
 export const getMdxBySlug = async (sourcePath, slug) => {
-  const source = fs.readFileSync(
-    path.join(ROOT, sourcePath, `${slug}.mdx`),
-    "utf8"
-  );
+  const source = fs.readFileSync(path.join(ROOT, 'data', sourcePath, `${slug}.mdx`), 'utf8');
 
-  if (process.platform === "win32") {
+  if (process.platform === 'win32') {
     process.env.ESBUILD_BINARY_PATH = path.join(
       process.cwd(),
-      "node_modules",
-      "esbuild",
-      "esbuild.exe"
+      'node_modules',
+      'esbuild',
+      'esbuild.exe'
     );
   } else {
     process.env.ESBUILD_BINARY_PATH = path.join(
       process.cwd(),
-      "node_modules",
-      "esbuild",
-      "bin",
-      "esbuild"
+      'node_modules',
+      'esbuild',
+      'bin',
+      'esbuild'
     );
   }
   const { frontmatter, code } = await bundleMDX(source, {
     xdmOptions(options) {
       options.remarkPlugins = [
         ...(options.remarkPlugins ?? []),
-        require("remark-slug"),
+        require('remark-slug'),
         remarkSectionize,
         getImageBlurData,
       ];
