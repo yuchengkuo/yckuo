dae026bed8f7ef77f7511a7f2162f4089d250c4d
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-11-23 00:25:18 +0800

    update animation

--- utils/animation.ts
+++ utils/animation.ts
@@ -1,4 +1,4 @@
-import { Variants } from 'framer-motion';
+import { Transition, Variants } from 'framer-motion';
 
 export const scale: Variants = {
   0: { scale: 0 },
@@ -13,9 +13,17 @@ export const fade: Variants = {
 export const slideUp: Variants = {
   0: { y: '50%', opacity: 0 },
   1: { y: 0, opacity: 1 },
+  2: { y: '-50%', opacity: 0 },
 };
 
 export const slideDown: Variants = {
   0: { y: '-50%', opacity: 0 },
   1: { y: 0, opacity: 1 },
+  2: { y: '50%', opacity: 0 },
+};
+
+export const headingTransition: Transition = {
+  type: 'spring',
+  damping: 20,
+  mass: 1,
 };
