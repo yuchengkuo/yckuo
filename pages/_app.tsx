ecdeaf395b137b1994bec649e23a9b7a9d0f0aeb
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-11-28 21:08:45 +0800

    update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,12 @@ import Header from '@components/Header';
 import Footer from '@components/Footer';
 import Cursor from '@components/Cursor';
 import { Menu } from '@components/Menu';
-import Head from '@components/Head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo />
-      <Head />
       <ThemeProvider attribute="class">
         <Menu />
         <Cursor />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,12 @@ export default class MyDocument extends Document {
           ></script>
 
           <link
+            rel="icon"
+            type="image/svg+xml"
+            sizes="16x16"
+            href="data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cstyle%3E @media (prefers-color-scheme: dark) %7B :root %7B filter: invert(100%25); %7D %7D %3C/style%3E%3Cpath d='M12 8V24M12 8H4L8 16L4 24H12M12 8H20M12 24H20M12 24L20 8M24 16.3333L28 8H20M24 16.3333L28 24H20M24 16.3333L20 24' stroke='%23282828' stroke-width='1.8' stroke-linejoin='round'/%3E%3C/svg%3E%0A"
+          />
+          <link
             rel="apple-touch-icon"
             sizes="180x180"
             href="/static/favicons/apple-touch-icon.png"
