3bb2f58d14e25091c0cdff29f43e52eeb89cf7bf
YuCheng Kuo <yuchengkuoo@gmail.com>
2021-12-01 00:09:21 +0800

    update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,22 @@
 import { m } from 'framer-motion';
 import Layout from '@components/layout/Layout';
+import { allOthers } from '.contentlayer/data';
+import { InferGetStaticPropsType } from 'next';
 
-export default function AboutPage() {
+export default function AboutPage({ about }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
       <div className="">
         <m.h1 className="">About</m.h1>
+
+        <article dangerouslySetInnerHTML={{ __html: allOthers[0].body.html }}></article>
       </div>
     </Layout>
   );
+}
+
+export function getStaticProps() {
+  console.log(allOthers);
+  const about = allOthers.find((page) => page.title === 'About');
+  return { props: { about } };
 }
