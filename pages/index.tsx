3441501b39156d80313df70e93211766be1b8ecb
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-01-10 23:01:04 +0800

    update link styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,12 @@ import Layout from '@components/layout/Layout'
 export default function Home() {
   return (
     <Layout title="YuCheng Kuo" subtitle="Digital Designer, Engineer" animateChildren>
-      <p className="text-base font-freak mt-4 text-gray8 dark:text-gray6">
-        Currently @{' '}
+      <p className="text-lg phone:text-base body-font-settings mt-4 max-w-[500px] text-gray9 dark:text-gray5">
+        Hey, I'm YuCheng. I am a self-taught designer and engineer from Taiwan. I'm currently
+        helping building{' '}
         <a
           href="https://oen.tw"
-          className="hover:bg-gray7/30 hover:underline rounded px-0.5 -mx-0.5"
+          className="underline decoration-gray7 hover:decoration-inherit transition-all duration-200 ease-out"
         >
           Oen.tw
         </a>
