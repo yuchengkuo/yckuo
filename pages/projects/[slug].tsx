8a4dad35cf7b7933f64f12a57404cdf98a2e0558
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-01-29 18:27:58 +0800

    remove project page for now

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,26 +1,3 @@
-import { allProjects } from '.contentlayer/data'
-import { InferGetStaticPropsType } from 'next'
-import { getMDXComponent } from 'mdx-bundler/client'
-import ProjectLayout from '@components/layout/ProjectLayout'
-
-export default function ProjectPage({ project }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const Content = getMDXComponent(project.body.code)
-  return (
-    <ProjectLayout key={project.slug} project={project}>
-      <Content />
-    </ProjectLayout>
-  )
-}
-
-const CarouselImg = () => {
+export default function ProjectPage() {
   return <></>
-}
-
-export async function getStaticProps({ params }) {
-  const project = allProjects.find((p) => p.slug === params.slug)
-  return { props: { project } }
-}
-
-export function getStaticPaths() {
-  return { paths: allProjects.map((p) => ({ params: { slug: p.slug } })), fallback: false }
 }
