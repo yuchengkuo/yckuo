96b362865cea24b11ffe203944eb094f66d5888e
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-03-13 13:45:09 +0800

    fix script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <Script async data-api="/_hive" src="/bee.js"></Script>
+          <script async data-api="/_hive" src="/bee.js"></script>
         </Head>
         <body>
           <Main />
