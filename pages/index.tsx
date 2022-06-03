f3f2931e9e58c677ba864f946fc1c7358125c0a3
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-06-03 17:39:51 +0800

    chore: add prettier for tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import Layout from '@components/layout/Layout'
 export default function Home() {
   return (
     <Layout title="YuCheng Kuo" subtitle="Digital Designer, Engineer" animateChildren>
-      <p className="body-font-settings max-w-[500px]">
+      <p className="max-w-[500px] body-font-settings">
         Hey, I&apos;m YuCheng. I am a self-taught designer and engineer from Taiwan. I&apos;m
         currently helping building{' '}
         <a
           href="https://oen.tw"
-          className="underline decoration-gray-9 dark:decoration-grayDark-9 hover:decoration-inherit transition-all duration-200 ease-out"
+          className="underline decoration-gray-9 transition-all duration-200 ease-out hover:decoration-inherit dark:decoration-grayDark-9"
         >
           Oen.tw
         </a>
