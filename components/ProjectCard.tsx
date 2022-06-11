ee89186655d6efceda091c52364c3cb9b507ae8b
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-06-11 13:53:06 +0800

    refine: update project layout

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -6,6 +6,7 @@ import { RightArrowIcon, UpRightArrowIcon } from './Icons'
 import Image from 'next/image'
 import { loader } from '@utils/image-loader'
 import splitbee from '@splitbee/web'
+import classNames from 'classnames'
 
 export default function ProjectCard({ project }: { project: Project | ProjectLink }) {
   return (
@@ -39,36 +40,28 @@ export default function ProjectCard({ project }: { project: Project | ProjectLin
           </p>
         </div>
         <div className="mt-2 flex gap-4">
-          {project.cover && (
-            <div className="bg-gray-3 dark:bg-grayDark-3 border-gray-6 dark:border-grayDark-6 relative h-40 w-2/3 rounded border shadow">
-              <Image
-                src={project.cover[0]}
-                alt=""
-                loader={loader}
-                placeholder="blur"
-                blurDataURL={project.type === 'Project' && project.blurDataURL[0]}
-                objectFit="cover"
-                layout="fill"
-                sizes="30vw"
-                className="rounded"
-              />
-            </div>
-          )}
-          {project.cover && (
-            <div className="bg-gray-3 dark:bg-grayDark-3 border-gray-6 dark:border-grayDark-6 relative h-40 w-1/3 rounded border shadow">
-              <Image
-                src={project.cover[1]}
-                alt=""
-                loader={loader}
-                placeholder="blur"
-                blurDataURL={project.type === 'Project' && project.blurDataURL[1]}
-                objectFit="cover"
-                layout="fill"
-                sizes="30vw"
-                className="rounded"
-              />
-            </div>
-          )}
+          {project.cover &&
+            project.cover.map((cover, index) => (
+              <div
+                key={cover}
+                className={classNames(
+                  'bg-gray-3 dark:bg-grayDark-3 border-gray-6 dark:border-grayDark-6 relative h-40 rounded border shadow phone:h-28',
+                  Math.random() > 0.5 ? 'w-2/3' : 'w-full'
+                )}
+              >
+                <Image
+                  src={project.cover[index]}
+                  alt=""
+                  loader={loader}
+                  placeholder={'blurDataURL' in project ? 'blur' : 'empty'}
+                  blurDataURL={project.type === 'Project' && project.blurDataURL[index]}
+                  objectFit="cover"
+                  layout="fill"
+                  sizes="30vw"
+                  className="rounded"
+                />
+              </div>
+            ))}
         </div>
       </m.a>
     </Link>
