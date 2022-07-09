3945f966e4397b1b37e5164135347c172f44eaf6
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-07-10 00:33:57 +0800

    refine: update root layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -15,19 +15,25 @@
 </script>
 
 {#key $page}
-  <main use:motion={{ keyframes: fade, options: { duration: 0.4 } }} class="h-full mb-auto p-20">
+  <main
+    use:motion={{ keyframes: fade, options: { duration: 0.4, delay: 0.4 } }}
+    class="h-full mb-auto p-20"
+  >
     {#if $page.url.pathname !== '/'}
-      <a class="mb-10" href="/">← Go back</a>
+      <button
+        class="font-Azeret font-450 text-sm text-$colors-fg-secondary mb-8 transition-all duration-200 uppercase underline underline-$colors-border decoration-1.6px underline-offset-1.4px hover:(decoration-2.2px underline-current) "
+        on:click={() => window.history.back()}>←·Back</button
+      >
     {/if}
 
     <slot />
   </main>
 {/key}
 
-<footer class="flex px-20 pb-10 justify-between items-baseline">
-  <div>
-    <h3 class="font-650">YuCheng Kuo</h3>
-    <p class="font-Azeret mt-1 text-xs">(C)2022</p>
+<footer class="flex mt-40 text-$colors-fg-secondary px-6 pb-4 justify-between items-baseline">
+  <h3 class="font-600 text-base text-$colors-fg-secondary tracking-tight">YuCheng Kuo</h3>
+  <div class="flex font-Azeret text-xs gap-6">
+    <p class="slashed-zero">(C)2019-present</p>
+    <a href="/" class="uppercase">Changelog</a>
   </div>
-  <nav class="flex gap-12" />
 </footer>
