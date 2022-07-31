260e2a3fe270e556d412f1e860bd49732b544b12
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-07-31 14:02:39 +0800

    feat: seo head

---
+++ src/components/Head.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import SvelteSeo from 'svelte-seo'
+  const title = 'YuCheng Kuo · Designer x Engingeer'
+  const description =
+    'Personal website, portfolio, little corner on the internet of YuCheng Kuo, a product designer and UI developer'
+</script>
+
+<SvelteSeo
+  {title}
+  {description}
+  canonical="https://yuchengkuo.com/"
+  openGraph={{ type: 'website', url: 'https://yuchengkuo.com', title, description }}
+  {...$$props}
+>
+  <slot />
+</SvelteSeo>
