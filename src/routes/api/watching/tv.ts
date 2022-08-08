679953f68f9d202f7af8e72d7bf9bfa823cd0e10
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-08-08 21:01:46 +0800

    feat: watching route

---
+++ src/routes/api/watching/tv.ts
@@ -0,0 +1,45 @@
+import { getFavoriteTV, getPosterUrl, getShowDetail } from '$lib/api/moviedb'
+import type { RequestHandler } from '@sveltejs/kit'
+import { compareDesc } from 'date-fns'
+
+export const GET: RequestHandler = async function () {
+  const res = await getFavoriteTV()
+
+  if (!res.ok) {
+    return {
+      status: 404,
+    }
+  }
+
+  const { results, total_pages } = await res.json()
+
+  if (total_pages > 1) {
+    let pages = total_pages
+    do {
+      const { results: more } = await (await getFavoriteTV(pages)).json()
+      results.push(...more)
+    } while (--pages > 1)
+  }
+
+  return {
+    status: 200,
+    body: {
+      results: (
+        await Promise.all(
+          results.map(async (show) => ({
+            title: show.name,
+            posterUrl: await getPosterUrl(show.poster_path),
+            time: show.first_air_date,
+            overview: show.overview,
+            vote: show.vote_average,
+            id: show.id,
+            ...(await getShowDetail(show.id)),
+          }))
+        )
+      ).sort((a, b) => compareDesc(new Date(a.lastAir), new Date(b.lastAir))),
+    },
+    headers: {
+      'Cache-Control': 'public, s-maxage=1080000, stale-while-revalidate=43200',
+    },
+  }
+}
---
+++ src/routes/watching.svelte
@@ -0,0 +1,87 @@
+<script lang="ts" context="module">
+  import type { Load } from '@sveltejs/kit'
+
+  export const load: Load = async function ({ fetch }) {
+    const res = await fetch('/api/watching/tv')
+
+    if (res.ok) {
+      return {
+        status: 200,
+        props: { shows: (await res.json()).results },
+        cache: { maxage: 15 * 24 * 60 * 60, private: false },
+      }
+    }
+  }
+</script>
+
+<script lang="ts">
+  import Head from '$components/Head.svelte'
+
+  import Image from '$lib/image/Image.svelte'
+
+  type Show = {
+    [key: string]: string
+  }
+
+  let hoverId: number
+  let x: number
+  let y: number
+
+  export let shows: Show[]
+</script>
+
+<Head title="Watching · YuCheng Kuo">
+  <script>
+    document.documentElement.setAttribute('data-theme', 'orange')
+  </script>
+</Head>
+
+<h1 class="mb-50">Watching</h1>
+
+<section class="*grid">
+  <ul class="grid gap-x-8 gap-y-16 grid-cols-5 col-span-6">
+    {#each shows as show, index (show.id)}
+      <li
+        class="group"
+        on:mouseenter={() => (hoverId = index)}
+        on:focus={() => (hoverId = index)}
+        on:mouseleave={() => (hoverId = undefined)}
+        on:blur={() => (hoverId = undefined)}
+        on:mousemove={(e) => {
+          x = e.clientX + 8
+          y = e.clientY + 8
+        }}
+        tabindex="0"
+      >
+        <a
+          class="w-full block hover:(scale-101 -translate-y-1 rotate-0.5 shadow-md) active:(scale-99) group-focus:(scale-101 -translate-y-1 rotate-0.5 shadow-md) "
+          href={`https://www.themoviedb.org/tv/${show.id}`}
+        >
+          <Image
+            id={show.posterUrl}
+            alt=""
+            widths={[120, 240, 360]}
+            class="rounded-md shadow-smoverflow-hidden aspect-100/66"
+          />
+        </a>
+        <h4 class="mt-4 text-center line-clamp-2">{show.title}</h4>
+        <div class="flex flex-col text-fg-secondary text-sm items-center justify-center">
+          <time class="font-Azeret ">{show.time.substring(0, 4)}</time>
+          <p class="text-xs">{show.status}</p>
+        </div>
+      </li>
+    {/each}
+  </ul>
+</section>
+
+{#if shows[hoverId]}
+  <div
+    class="rounded-md h-max bg-fg-secondary/84 shadow-lg text-bg p-4 transform w-80 rotate-2 backdrop-blur-lg backdrop-filter fixed pointer-events-none will-change-auto"
+    style={`left: ${x}px; top: ${y}px;`}
+  >
+    {#key hoverId}
+      <h4 class="font-575 text-bg mb-1">{shows[hoverId].title}</h4>
+      <p class="text-base">{shows[hoverId].overview}</p>
+    {/key}
+  </div>
+{/if}
