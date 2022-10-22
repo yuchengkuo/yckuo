7db6c889183eb40a8408deab736779cf62caea8f
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-10-22 23:42:02 +0800

    feat: add animation for bookmark

--- src/routes/bookmark/+page.svelte
+++ src/routes/bookmark/+page.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
+  import { spring } from 'motion'
   import Head from '$lib/seo/Head.svelte'
   import Image from '$lib/image/Image.svelte'
+  import { motion } from '$lib/animation/motion'
 
   import type { PageData } from './$types'
 
   export let data: PageData
+
+  const cord = { x: 0, y: 0 }
+  const scrollOffset = 24
+  let initialScrollTop = 0
+  let downscroll = true
 </script>
+
+<svelte:window
+  on:scroll={() => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    scrollTop > initialScrollTop ? (downscroll = true) : (downscroll = false)
+    initialScrollTop = scrollTop <= 0 ? 0 : scrollTop
+  }}
+/>
 
 <Head
   title="Bookmarks · YuCheng Kuo"
@@ -27,12 +42,34 @@
       {@const deg = Math.floor((Math.random() * 20 - 10) % 3)}
       <li
         class="relative group sm:(items-baseline grid gap-x-4 grid-cols-[2fr_1fr]) md:grid-cols-[2fr_1fr_1fr] xl:(grid-cols-[2fr_3fr_3fr] gap-x-10) "
+        on:mousemove={(e) => {
+          const offset = 56
+          const left = e.currentTarget.offsetLeft
+          const width = e.currentTarget.clientWidth
+          const top = e.currentTarget.offsetTop
+          const height = e.currentTarget.clientHeight
+          const offsetX = ((e.pageX - left - width / 2) / width) * offset
+          const offsetY = ((e.pageY - top - height / 2) / height) * offset
+          cord.x = offsetX
+          cord.y = offsetY
+        }}
+        on:mouseleave={() => {
+          cord.x = 0
+          cord.y = 0
+        }}
+        use:motion={{
+          initial: { y: downscroll ? scrollOffset : -scrollOffset },
+          inView: { y: 0 },
+          transition: { easing: spring({ damping: 30, stiffness: 600 }) },
+        }}
       >
         <div
-          class="flex flex-col gap-1 hidden md:(block opacity-100) xl:(justify-self-end items-end opacity-0) <xl:order-2 group-hover:opacity-100 "
+          class="hidden md:(block opacity-100) xl:opacity-0 <xl:order-2 group-hover:opacity-100 "
         >
           {#each item.tags as tag}
-            <p class="font-Azeret font-400 text-fg-secondary text-xs xl:text-right">
+            <p
+              class="font-Azeret font-400 text-fg-secondary text-xs xl:text-right xl:not-last:mb-1"
+            >
               – {tag}
             </p>
           {/each}
@@ -47,6 +84,10 @@
         <div
           class="mt-2 opacity-100 md:mt-0 xl:(opacity-0 w-7/10 h-0 justify-self-end) <xl:order-3 group-hover:opacity-100 "
           style:transform="rotate({deg}deg)"
+          use:motion={{
+            animate: { x: cord.x, y: cord.y },
+            transition: { easing: spring({ damping: 40 }) },
+          }}
         >
           <Image
             id={item.cover}
