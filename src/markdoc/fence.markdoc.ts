e0d3a43b3e481023a29298cf7230937e2c960e6f
YuCheng Kuo <yuchengkuoo@gmail.com>
2022-11-09 23:03:50 +0800

    feat: font style and highlight in `pre`

--- src/markdoc/fence.markdoc.ts
+++ src/markdoc/fence.markdoc.ts
@@ -1,6 +1,6 @@
 import pkg, { type Schema } from '@markdoc/markdoc'
 const { Tag } = pkg
-import shiki, { type IThemedToken } from 'shiki'
+import shiki, { FontStyle, type IThemedToken } from 'shiki'
 
 export const fence: Schema = {
   render: 'Fence',
@@ -9,6 +9,7 @@ export const fence: Schema = {
     content: { type: String, render: false },
     language: { type: String },
     process: { type: Boolean, render: false },
+    highlight: { type: Array },
   },
   async transform(node, config) {
     const attributes = node.transformAttributes(config)
@@ -26,12 +27,13 @@ export const fence: Schema = {
 
     const lang = attributes.language || 'text'
     const code = typeof children[0] === 'string' && children[0]
+    const highlight = attributes.highlight as Array<number | Array<number>>
 
     const tokens = highlighter.codeToThemedTokens(code, lang)
     const darkToens = highlighter.codeToThemedTokens(code, lang, theme.dark)
 
-    const lightTree = getRenderableTree(tokens)
-    const darkTree = getRenderableTree(darkToens)
+    const lightTree = getRenderableTree(tokens, highlight)
+    const darkTree = getRenderableTree(darkToens, highlight)
     const lightBG = highlighter.getBackgroundColor()
     const darkBG = highlighter.getBackgroundColor(theme.dark)
 
@@ -53,19 +55,39 @@ export const fence: Schema = {
   },
 }
 
-function getRenderableTree(tokens: IThemedToken[][]) {
+function getRenderableTree(tokens: IThemedToken[][], highlights?: (number | number[])[]) {
+  const lines = highlights?.filter((h) => !Array.isArray(h)) as number[]
+  const ranges = (highlights?.filter((h) => Array.isArray(h)) as number[][])?.map((h) => {
+    if (h.length !== 2) throw Error('Highlight range must be in ["start", "end"] format')
+    const start = Math.min(h[0], h[1])
+    const end = Math.max(h[0], h[1])
+    return [start, end]
+  })
+
   return new Tag(
     'code',
     { class: 'inline' },
-    tokens.map(
-      (tokenArr) =>
-        new Tag(
-          'div',
-          { class: 'line' },
-          tokenArr.map(
-            (token) => new Tag('span', { style: `color: ${token.color}` }, [token.content])
-          )
-        )
-    )
+    tokens.map((tokenArr, index) => {
+      const target = index + 1
+      const highlight = lines?.includes(target) ? 'highlight' : ''
+      const range = ranges?.some((range) => target >= range[0] && target <= range[1])
+        ? 'highlight-range'
+        : ''
+      const rangeStart = ranges?.some((range) => target === range[0]) ? 'highlight-start' : ''
+      const rangeEnd = ranges?.some((range) => target === range[1]) ? 'highlight-end' : ''
+      return new Tag(
+        'div',
+        { class: ['line', highlight, range, rangeStart, rangeEnd].join(' ') },
+        tokenArr.map((token) => {
+          const { color, content, fontStyle } = token
+          const text = `color: ${color}`
+          const bold = fontStyle === FontStyle['Bold'] && 'font-weight: bold;'
+          const italic = fontStyle === FontStyle['Italic'] && 'font-style: italic;'
+          const underline = fontStyle === FontStyle['Underline'] && 'text-decoration: underline;'
+          const style = [text, bold, italic, underline].filter((i) => i).join(' ')
+          return new Tag('span', { style }, [content])
+        })
+      )
+    })
   )
 }
