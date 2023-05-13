ddc40f966e31ec8bb6d6fc0cc7f397a4f0747a7d
YuCheng Kuo <yuchengkuoo@gmail.com>
2023-05-13 21:35:00 +0800

    fix: date format

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,5 +1,5 @@
 export function formatDate(date: string | number | Date, options: Intl.DateTimeFormatOptions = {}) {
-  if (!options) options.dateStyle = 'medium'
+  if (!Object.keys(options).length) options.dateStyle = 'medium'
   return new Intl.DateTimeFormat('en-US', {
     timeZone: 'Asia/Taipei',
     ...options,
