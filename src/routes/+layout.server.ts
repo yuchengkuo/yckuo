2002826b170806008a0bee5b6451529f0fa4ad41
YuCheng Kuo <yuchengkuoo@gmail.com>
2025-07-19 00:06:03 +0800

    fix: data fetching in layout and page

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,18 +1,11 @@
 export const prerender = true
 
-import type { Navigation, Project, Work } from '$content'
+import type { Navigation } from '$content'
 
 export async function load({ fetch }) {
   const { navigation } = (await fetch('/api/content/entry/navigation').then((res) =>
     res.json()
   )) as Navigation
 
-  const works = (await fetch('/api/content/collection/works/published:desc').then((res) =>
-    res.json()
-  )) as Work[]
-  const projects = (await fetch('/api/content/collection/projects').then((res) =>
-    res.json()
-  )) as Project[]
-
-  return { navigation, works, projects }
+  return { navigation }
 }
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,16 @@
 import type { Org, Page, Project, Work } from '$content'
 
-export async function load({ fetch, parent }) {
+export async function load({ fetch }) {
   const page = (await fetch('/api/content/entry/index').then((res) => res.json())) as Page
 
   const orgs = (await fetch('/api/content/collection/orgs').then((res) => res.json())) as Org[]
 
-  const { works, projects } = await parent()
+  const works = (await fetch('/api/content/collection/works/published:desc').then((res) =>
+    res.json()
+  )) as Work[]
+  const projects = (await fetch('/api/content/collection/projects').then((res) =>
+    res.json()
+  )) as Project[]
 
   return {
     ...page,
